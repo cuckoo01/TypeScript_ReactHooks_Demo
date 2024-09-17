b3011c947e2aa0b36e421d90007a52b99c524a1d
DuyenNguyen <duyenntbse183629@fpt.edu.vn>
2024-09-17 13:25:15 +0700

    second trial commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     return (
         <>
             <h1>TypeScript + Javascript Demo</h1>
+            <h1>Hello World</h1>
             <UseCallBackDemo />
             <UseContextDemo />
         </>
